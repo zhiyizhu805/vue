66709a2fec8251493142c030c27deacd740de8cd
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2024-01-01 16:54:29 +1300

    routing

--- http-01-starting-setup/src/components/survey/LearningSurvey.vue
+++ http-01-starting-setup/src/components/survey/LearningSurvey.vue
@@ -9,7 +9,13 @@
         </div>
         <h3>My learning experience was ...</h3>
         <div class="form-control">
-          <input type="radio" id="rating-poor" value="poor" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-poor"
+            value="poor"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-poor">Poor</label>
         </div>
         <div class="form-control">
@@ -23,12 +29,20 @@
           <label for="rating-average">Average</label>
         </div>
         <div class="form-control">
-          <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-great"
+            value="great"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-great">Great</label>
         </div>
-        <p
-          v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="invalidInput">
+          One or more input fields are invalid. Please check your provided data.
+        </p>
+        <!-- step(5.4) show an error message if the request fails -->
+        <p v-if="error">{{ error }}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -44,9 +58,11 @@ export default {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      // step(5.1) add a property to store an error message for sending data
+      error: null,
     };
   },
-  emits: ['survey-submit'],
+  // emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -54,11 +70,43 @@ export default {
         return;
       }
       this.invalidInput = false;
+      // (5.2)set error to null before sending data
+      this.error = null;
+      // this.$emit('survey-submit', {
+      //   userName: this.enteredName,
+      //   rating: this.chosenRating,
+      // });
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
+      //Sending data
+      //step 1: add fetch api to send a request to firebase.You can choose any identifier, and Firebase will use it to create a database entry and store our data under that identifier. Append the 'surveys' identifier at the end, as required by Firebase, and then add '.json'.
+      // method: 'PUT' // method: 'PATCH' // method: 'DELETE' // method: 'GET' // method: 'POST'
+      //step 2: by default,this is a GET request,so we need to add a second argument to configure the request.the second argument is an javascript object where we can configure the request.
+      //step 3: we need to add a header to the request,so we can tell firebase that we are sending json data.
+      //step 4: we need to add a body to the request,so we can send the data to firebase.  The value you tried to emit in the past.
+      //use josn.stringify to convert the object to a json string.
+      fetch(
+        'https://vue-http-demo-74044-default-rtdb.firebaseio.com/surveys.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: this.enteredName,
+            rating: this.chosenRating,
+          }),
+        }
+      )
+        .then((response) => {
+          //step(6) add a response.ok check to check is there any server side error
+          if (!response.ok) {
+            throw new Error('Sending survey failed!');
+          }
+        })
+        .catch((error) => {
+          //step(5.3) add an error handler
+          this.error = error.message;
+        });
 
       this.enteredName = '';
       this.chosenRating = null;
--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,9 +3,12 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else-if="!isLoading && results && results.length != 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -13,6 +16,10 @@
           :rating="result.rating"
         ></survey-result>
       </ul>
+      <!-- step(4.4) show an error message if the request fails -->
+      <p v-else-if="!isLoding&&error">{{error}}</p>
+      <!--  step(4.3) show a message if there are no stored experiences -->
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored experiences found.Start adding some!</p>
     </base-card>
   </section>
 </template>
@@ -21,9 +28,83 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  // props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      // Step(3) show a loading text while the data is being fetched
+      isLoading: false,
+      // step(4.1) add a property to store an error message
+      error: null,
+    };
+  },
+  // Ste(2) fetch data from the server
+  methods: {
+    // {method: 'GET'} //the second argument is an object with a method property set to GET by default so we can omit it
+    loadExperiences() {
+      this.isLoading = true;
+      //1. send http request
+      //2. response.json() returns a promise too,so we can chain another then() block to handle the response data
+      //3. we can(not mandatory) to transform the response data into an array of objects
+      //4. assign the transformed data to the results property
+      //5. use arrow function to make sure the this keyword is pointing to the vue instance object
+      fetch(
+        'https://vue-http-demo-74044-default-rtdb.firebaseio.com/surveys.json'
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then((data) => {
+          //when we have the data,we can set the isLoading property to false
+          this.isLoading = false;
+          // console.log(data);
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          // pass the results to the data property
+          // this keyword is not pointing to the vue instance object. so we need to use arrow function since arrow function does not have its own this keyword,instead it uses the this keyword of its closest parent scope.
+          this.results = results;
+        }).catch(error => {
+          //step(4.2) catch any errors that might occur and set the error property
+          this.isLoading = false;
+          console.log(error);
+          this.error = 'Fail to fetch data - please try again later.'
+        });
+
+      // fetch(
+      //   'https://vue-http-demo-74044-default-rtdb.firebaseio.com/surveys.json'
+      // )
+      //   .then(function (response) {
+      //     return response.json();
+      //   })
+      //   .then(function (data) {
+      //     console.log(data);
+      //     const results = [];
+      //     for (const id in data) {
+      //       results.push({
+      //         id: id,
+      //         name: data[id].name,
+      //         rating: data[id].rating,
+      //       });
+      //     }
+      //     // pass the results to the data property
+      //     //❌ this keyword is not pointing to the vue instance object. so we need to use arrow function since arrow function does not have its own this keyword,instead it uses the this keyword of its closest parent scope.
+      //     this.results = results;
+      //   });
+        
+    },
+  },
+  //trigger the loadExperiences method when the component is mounted
+  mounted() {
+    this.loadExperiences();
   },
 };
 </script>
